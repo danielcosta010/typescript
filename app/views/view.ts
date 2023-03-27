fe285ce88d212c55412e8cd004e8446560a4ff2f
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-03-27 14:58:46 -0300

    abstract class
    o template comente poderá ser implementado nas classes filhas
    
      abstract template(model: T): string

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,13 +1,12 @@
-export class View<T> {
+export abstract class View<T> {
+  
   protected elemento: HTMLElement;
 
   constructor(seletor: string) {
     this.elemento = document.querySelector(seletor);
   }
 
-  template(model: T): string {
-    throw Error('Classe filha precisa implementar o template')
-  }
+  abstract template(model: T): string 
 
   update(model: T): void {
     const template = this.template(model);
