33fb8c7d9f9fc9d64ab23456cc35bab9d62ee9a8
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-03-28 00:12:57 -0300

    Update view.ts

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,14 +1,14 @@
 export abstract class View<T> {
-  
+
   protected elemento: HTMLElement;
 
   constructor(seletor: string) {
     this.elemento = document.querySelector(seletor);
   }
 
-  abstract template(model: T): string 
+  protected abstract template(model: T): string 
 
-  update(model: T): void {
+  public update(model: T): void {
     const template = this.template(model);
     this.elemento.innerHTML = template;
   }
