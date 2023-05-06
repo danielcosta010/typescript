065d88c3b58cb435c73727b148844288036b01d0
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-05-06 15:58:26 -0300

    Update negociacao-controller.ts

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,3 +1,5 @@
+import { domInjector } from "../decorators/dom-injector.js";
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logarTempoDeExecucao.js";
 import { DiasDaSemana } from "../enums/dias-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
@@ -6,8 +8,11 @@ import { MensagemView } from "../views/mensagem-view.js";
 import { NegociacoesView } from "../views/negociacoes-view.js";
 
 export class NegociacaoController {
+  @domInjector('#data')
   private inputData: HTMLInputElement;
+  @domInjector('#quantidade')
   private inputQuantidade: HTMLInputElement;
+  @domInjector('#valor')
   private inputValor: HTMLInputElement;
   private negociacoes = new Negociacoes();
   private negociacoesView = new NegociacoesView('#negociacoesView');
@@ -15,12 +20,10 @@ export class NegociacaoController {
   
 
   constructor() {
-    this.inputData = <HTMLInputElement>document.querySelector('#data');
-    this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
-    this.inputValor= document.querySelector('#valor') as HTMLInputElement;
     this.negociacoesView.update(this.negociacoes);
   }
 
+  @inspect
   @logarTempoDeExecucao()
   public adiciona(): void {
     /* Comentário não pode aparecer no arquivo js */
